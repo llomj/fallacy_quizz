c011baff1f5e50e449a0c2ee262cfb8985f755e7
llomj <jonllom80@protonmail.com>
2026-07-16 10:37:10 +1100

    Activate level 8 beginner question overrides

--- src/questionsBank.ts
+++ src/questionsBank.ts
@@ -18,6 +18,7 @@ import { LEVEL_6_OVERRIDES_EN, LEVEL_6_OVERRIDES_FR } from './data/questions/lev
 import { LEVEL_6_INTERMEDIATE_OVERRIDES_EN, LEVEL_6_INTERMEDIATE_OVERRIDES_FR } from './data/questions/level6IntermediateOverrides';
 import { LEVEL_6_EXPERT_OVERRIDES_EN, LEVEL_6_EXPERT_OVERRIDES_FR } from './data/questions/level6ExpertOverrides';
 import { LEVEL_7_OVERRIDES_EN, LEVEL_7_OVERRIDES_FR } from './data/questions/level7Overrides';
+import { LEVEL_8_OVERRIDES_EN, LEVEL_8_OVERRIDES_FR } from './data/questions/level8Overrides';
 
 function applyQuestionOverrides(base: Question[], overrides: Question[]): Question[] {
   const overrideMap = new Map(overrides.map((question) => [question.id, question]));
@@ -50,6 +51,7 @@ const FALLACY_GAME_IDS_ONLY_EN = applyAllQuestionOverrides(
     LEVEL_6_INTERMEDIATE_OVERRIDES_EN,
     LEVEL_6_EXPERT_OVERRIDES_EN,
     LEVEL_7_OVERRIDES_EN,
+    LEVEL_8_OVERRIDES_EN,
   ]
 );
 const FALLACY_GAME_IDS_ONLY_FR = applyAllQuestionOverrides(
@@ -72,6 +74,7 @@ const FALLACY_GAME_IDS_ONLY_FR = applyAllQuestionOverrides(
     LEVEL_6_INTERMEDIATE_OVERRIDES_FR,
     LEVEL_6_EXPERT_OVERRIDES_FR,
     LEVEL_7_OVERRIDES_FR,
+    LEVEL_8_OVERRIDES_FR,
   ]
 );
 
