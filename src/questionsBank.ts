8645e227ec3d7722133cbcee8109697b68534d3d
llomj <jonllom80@protonmail.com>
2026-07-16 21:46:45 +1100

    wire appeal to consequences expansion packs into question bank

--- src/questionsBank.ts
+++ src/questionsBank.ts
@@ -6,6 +6,8 @@ import { APPEAL_TO_AUTHORITY_PACK_2_EN, APPEAL_TO_AUTHORITY_PACK_2_FR } from './
 import { APPEAL_TO_AUTHORITY_PACK_3_EN, APPEAL_TO_AUTHORITY_PACK_3_FR } from './data/questions/fallacyExpansions/appealToAuthorityPack3';
 import { APPEAL_TO_AUTHORITY_PACK_4_EN, APPEAL_TO_AUTHORITY_PACK_4_FR } from './data/questions/fallacyExpansions/appealToAuthorityPack4';
 import { APPEAL_TO_AUTHORITY_PACK_5_EN, APPEAL_TO_AUTHORITY_PACK_5_FR } from './data/questions/fallacyExpansions/appealToAuthorityPack5';
+import { APPEAL_TO_CONSEQUENCES_PACK_1_EN, APPEAL_TO_CONSEQUENCES_PACK_1_FR } from './data/questions/fallacyExpansions/appealToConsequencesPack1';
+import { APPEAL_TO_CONSEQUENCES_PACK_2_EN, APPEAL_TO_CONSEQUENCES_PACK_2_FR } from './data/questions/fallacyExpansions/appealToConsequencesPack2';
 import { APPEAL_TO_IGNORANCE_PACK_1_EN, APPEAL_TO_IGNORANCE_PACK_1_FR } from './data/questions/fallacyExpansions/appealToIgnorancePack1';
 import { APPEAL_TO_IGNORANCE_PACK_2_EN, APPEAL_TO_IGNORANCE_PACK_2_FR } from './data/questions/fallacyExpansions/appealToIgnorancePack2';
 import { APPEAL_TO_IGNORANCE_PACK_3_EN, APPEAL_TO_IGNORANCE_PACK_3_FR } from './data/questions/fallacyExpansions/appealToIgnorancePack3';
@@ -127,6 +129,8 @@ const FALLACY_GAME_IDS_ONLY_EN = applyAllQuestionOverrides(
     APPEAL_TO_AUTHORITY_PACK_3_EN,
     APPEAL_TO_AUTHORITY_PACK_4_EN,
     APPEAL_TO_AUTHORITY_PACK_5_EN,
+    APPEAL_TO_CONSEQUENCES_PACK_1_EN,
+    APPEAL_TO_CONSEQUENCES_PACK_2_EN,
     APPEAL_TO_IGNORANCE_PACK_1_EN,
     APPEAL_TO_IGNORANCE_PACK_2_EN,
     APPEAL_TO_IGNORANCE_PACK_3_EN,
@@ -235,6 +239,8 @@ const FALLACY_GAME_IDS_ONLY_FR = applyAllQuestionOverrides(
     APPEAL_TO_AUTHORITY_PACK_3_FR,
     APPEAL_TO_AUTHORITY_PACK_4_FR,
     APPEAL_TO_AUTHORITY_PACK_5_FR,
+    APPEAL_TO_CONSEQUENCES_PACK_1_FR,
+    APPEAL_TO_CONSEQUENCES_PACK_2_FR,
     APPEAL_TO_IGNORANCE_PACK_1_FR,
     APPEAL_TO_IGNORANCE_PACK_2_FR,
     APPEAL_TO_IGNORANCE_PACK_3_FR,
