9fc194c763f632c2ec25cdfe16c1551078a963bd
llomj <jonllom80@protonmail.com>
2026-07-16 11:32:19 +1100

    Activate further level 8 bias overrides

--- src/questionsBank.ts
+++ src/questionsBank.ts
@@ -28,6 +28,7 @@ import { LEVEL_8_MIXED_PART3_OVERRIDES_EN, LEVEL_8_MIXED_PART3_OVERRIDES_FR } fr
 import { LEVEL_8_MIXED_PART4_OVERRIDES_EN, LEVEL_8_MIXED_PART4_OVERRIDES_FR } from './data/questions/level8MixedPart4Overrides';
 import { LEVEL_8_MIXED_PART5_OVERRIDES_EN, LEVEL_8_MIXED_PART5_OVERRIDES_FR } from './data/questions/level8MixedPart5Overrides';
 import { LEVEL_8_MIXED_PART6_OVERRIDES_EN, LEVEL_8_MIXED_PART6_OVERRIDES_FR } from './data/questions/level8MixedPart6Overrides';
+import { LEVEL_8_MIXED_PART7_OVERRIDES_EN, LEVEL_8_MIXED_PART7_OVERRIDES_FR } from './data/questions/level8MixedPart7Overrides';
 
 function applyQuestionOverrides(base: Question[], overrides: Question[]): Question[] {
   const overrideMap = new Map(overrides.map((question) => [question.id, question]));
@@ -70,6 +71,7 @@ const FALLACY_GAME_IDS_ONLY_EN = applyAllQuestionOverrides(
     LEVEL_8_MIXED_PART4_OVERRIDES_EN,
     LEVEL_8_MIXED_PART5_OVERRIDES_EN,
     LEVEL_8_MIXED_PART6_OVERRIDES_EN,
+    LEVEL_8_MIXED_PART7_OVERRIDES_EN,
   ]
 );
 const FALLACY_GAME_IDS_ONLY_FR = applyAllQuestionOverrides(
@@ -102,6 +104,7 @@ const FALLACY_GAME_IDS_ONLY_FR = applyAllQuestionOverrides(
     LEVEL_8_MIXED_PART4_OVERRIDES_FR,
     LEVEL_8_MIXED_PART5_OVERRIDES_FR,
     LEVEL_8_MIXED_PART6_OVERRIDES_FR,
+    LEVEL_8_MIXED_PART7_OVERRIDES_FR,
   ]
 );
 
